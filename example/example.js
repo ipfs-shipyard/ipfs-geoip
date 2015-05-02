92e4588cd633f117617cfb13aa0aad0bff79fdae
Kristoffer Ström <kristoffer@rymdkoloni.se>
2015-05-02 21:04:53 +0200

    Packaging

---
+++ example/example.js
@@ -0,0 +1,10 @@
+var geoip = require('../')
+
+if (process.argv.length != 4) {
+	console.log("usage: node lookup.js root <ip4-adr>")
+	process.exit(1)
+}
+
+geoip.lookup(process.argv[2], process.argv[3], function (err, result) {
+	console.log(result)
+})
---
+++ ipfs-geoip.js
@@ -0,0 +1,36 @@
+var ipfs = require('ipfs-api')()
+
+function aton4 (a) {
+  a = a.split(/\./)
+  return ((parseInt(a[0], 10)<<24)>>>0) + ((parseInt(a[1], 10)<<16)>>>0) + ((parseInt(a[2], 10)<<8)>>>0) + (parseInt(a[3], 10)>>>0)
+}
+
+function _lookup (hash, lookfor, cb) {
+  ipfs.object.get(hash, function (err, res) {
+    var obj = JSON.parse(res.Data)
+
+    var child = 0;
+    if (obj.type == 'Node') {
+      while (obj.mins[child] < lookfor &&
+             obj.mins[child+1]) {
+        child++
+      }
+      return _lookup(res.Links[child-1].Hash, lookfor, cb)
+    } else if (obj.type == 'Leaf') {
+
+      while (obj.data[child].min < lookfor &&
+             obj.data[child+1]) {
+        child++
+      }
+
+      cb(null, obj.data[child-1].data)
+    }
+  })
+}
+
+function lookup (hash, ip, cb) {
+  _lookup(hash, aton4(ip), cb)
+}
+
+module.exports = {lookup: lookup,
+                  _lookup: _lookup}
