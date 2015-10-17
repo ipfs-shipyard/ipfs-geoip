9ab2a81ca2fbbfcfb6c6a354a519ba6bf7bb076d
dignifiedquire <dignifiedquire@gmail.com>
2015-10-17 12:33:05 +0200

    style: Happy linting

--- example/lookup.js
+++ example/lookup.js
@@ -1,15 +1,15 @@
 var geoip = require('../')
 var ipfs = require('ipfs-api')()
 
-if (process.argv.length != 3) {
-  console.log("usage: node lookup.js <ip4-adr>")
+if (process.argv.length !== 3) {
+  console.log('usage: node lookup.js <ip4-adr>')
   process.exit(1)
 }
 
 geoip.lookup(ipfs, process.argv[2], function (err, result) {
   if (err) {
-    console.log("Error: " + err)
+    console.log('Error: ' + err)
   } else {
-    console.log("Result: " + JSON.stringify(result, null, 2))
+    console.log('Result: ' + JSON.stringify(result, null, 2))
   }
 })
