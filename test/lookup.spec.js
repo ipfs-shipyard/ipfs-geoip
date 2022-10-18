e63110182a775c04074068af8a7ff008df3909d9
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-10-18 01:28:39 -0600

    Only using public gateway

--- test/lookup.spec.js
+++ test/lookup.spec.js
@@ -4,18 +4,18 @@ import * as geoip from '../src/index.js'
 describe('lookup via HTTP Gateway supporting application/vnd.ipld.raw responses', function () {
   this.timeout(100 * 1000)
 
-  const ipfs = process.env.CI ? 'https://ipfs.io' : 'http://127.0.0.1:8080'
+  const ipfsGW = 'https://ipfs.io'
 
   it('fails on 127.0.0.1', async () => {
     try {
-      await geoip.lookup(ipfs, '127.0.0.1')
+      await geoip.lookup(ipfsGW, '127.0.0.1')
     } catch (err) {
       expect(err).to.have.property('message', 'Unmapped range')
     }
   })
 
   it('looks up 66.6.44.4', async () => {
-    const result = await geoip.lookup(ipfs, '66.6.44.4')
+    const result = await geoip.lookup(ipfsGW, '66.6.44.4')
     expect(
       result
     ).to.be.eql({
@@ -33,14 +33,14 @@ describe('lookup via HTTP Gateway supporting application/vnd.ipld.raw responses'
   describe('lookupPretty', () => {
     it('fails on 127.0.0.1', async () => {
       try {
-        await geoip.lookupPretty(ipfs, '/ip4/127.0.0.1')
+        await geoip.lookupPretty(ipfsGW, '/ip4/127.0.0.1')
       } catch (err) {
         expect(err).to.have.property('message', 'Unmapped range')
       }
     })
 
     it('looks up 66.6.44.4', async () => {
-      const result = await geoip.lookupPretty(ipfs, '/ip4/66.6.44.4')
+      const result = await geoip.lookupPretty(ipfsGW, '/ip4/66.6.44.4')
       expect(
         result.formatted
       ).to.be.eql('Ashburn, VA, USA, Earth')
