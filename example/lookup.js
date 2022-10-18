d2e71392aa4dbd9e207c51c77a3a7a5176c30cf2
Marcin Rataj <lidel@lidel.org>
2022-10-18 15:35:01 +0200

    feat: support IPFS_GATEWAY env
    https://github.com/ipfs/specs/pull/280
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- example/lookup.js
+++ example/lookup.js
@@ -1,8 +1,7 @@
 import * as geoip from '../src/index.js'
 import { create } from 'ipfs-http-client'
 
-// This CLI tool requires Kubo RPC on 127.0.0.1:5001 to be running
-const ipfs = create(new URL('http://127.0.0.1:5001'))
+const ipfsGw = process?.env?.IPFS_GATEWAY || 'https://ipfs.io'
 
 if (process.argv.length !== 3) {
   console.log('usage: node lookup.js <ip4-adr>')
@@ -11,14 +10,14 @@ if (process.argv.length !== 3) {
 
 (async function() {
   try {
-    const result = await geoip.lookup(ipfs, process.argv[2])
+    const result = await geoip.lookup(ipfsGw, process.argv[2])
     console.log('Result: ' + JSON.stringify(result, null, 2))
   } catch (err) {
     console.log('Error: ' + err)
   }
 
   try {
-    const result = await geoip.lookupPretty(ipfs, '/ip4/' + process.argv[2])
+    const result = await geoip.lookupPretty(ipfsGw, '/ip4/' + process.argv[2])
     console.log('Pretty result: %s', result.formatted)
   } catch (err) {
     console.log('Error: ' + err)
--- test/lookup.spec.js
+++ test/lookup.spec.js
@@ -4,7 +4,7 @@ import * as geoip from '../src/index.js'
 describe('lookup via HTTP Gateway supporting application/vnd.ipld.raw responses', function () {
   this.timeout(100 * 1000)
 
-  const ipfsGW = 'https://ipfs.io'
+  const ipfsGW = process?.env?.IPFS_GATEWAY || 'https://ipfs.io'
 
   it('fails on 127.0.0.1', async () => {
     try {
